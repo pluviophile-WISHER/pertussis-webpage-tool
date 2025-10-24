7c064cb640d35845d95b67117f1be1f2a8cff779
pluviophile-WISHER <101853882+pluviophile-WISHER@users.noreply.github.com>
2025-10-24 08:26:52 +0800

    Update View_data_page.py

--- View_data_page.py
+++ View_data_page.py
@@ -9,17 +9,18 @@ from pathlib import Path
 def View_data_file():
     st.markdown('<span class="arrow">➤ View data</span> <p class="font"></p>', unsafe_allow_html=True)
     tab1, tab2, tab3 = st.tabs(["📖 Positive & Negative & Health", "🗃 Positive & Negative", "📑 Positive & Health"])
-    path_PNH = r'Pertussis data consolidation/positive, negative, physical examination.xlsx'  
+    path_PNH = r'Pertussis_data_consolidation/positive_negative_physical_examination.xlsx'  
     data_PNH = pd.read_excel(path_PNH)
     tab1.dataframe(data_PNH, width=1400, height=710) 
 
-    path_PN = r'Pertussis data consolidation/positive and negative.xlsx'
+    path_PN = r'Pertussis_data_consolidation/positive_and_negative.xlsx'
     data_PN = pd.read_excel(path_PN)
     tab2.dataframe(data_PN, width=1400, height=710)
     
-    path_PH = r'Pertussis data consolidation/positive and physical examination.xlsx'
+    path_PH = r'Pertussis_data_consolidation/positive_and_physical_examination.xlsx'
     data_PH = pd.read_excel(path_PH)
     tab3.dataframe(data_PH, width=1400, height=710)
+
 
 
 
