a171a52e8830d5be20e0124da26bff3c87e1ed71
pluviophile-WISHER <101853882+pluviophile-WISHER@users.noreply.github.com>
2025-10-27 15:23:10 +0800

    Update Add_data_page.py

--- Add_data_page.py
+++ Add_data_page.py
@@ -8,7 +8,7 @@ import pandas as pd
 from joblib import load
 from pathlib import Path
 import os
-import pkg_resources
+#import pkg_resources
 
 def Add_data_file():
     st.markdown('<span class="arrow">➤ Add data</span> <p class="font"></p>', unsafe_allow_html=True)
@@ -341,8 +341,8 @@ def Add_data_file():
             st.write(df.to_html(classes='dataframe', index=False, escape=False), unsafe_allow_html=True)
                     
         try:
-            model_path_PH = r"Pertussis data consolidation/PH_XGBoost_model1.joblib"  
-            model_PH = load_model(model_path_PH)
+            model_path_PH = r"Pertussis data consolidation/PH_XGBoost_model.joblib"  
+            model_PH = load(model_path_PH)
         except Exception as e:
             model_PH = None 
             st.error(f"{str(e)}")
@@ -483,6 +483,7 @@ def Add_data_file():
             return df
         if st.session_state.show_interpretation:   
             st.markdown("<h4 style='color: rgb(0, 168, 193); font-size: 35px;'>Analysis of blood routine test indicators</h4>", unsafe_allow_html=True)
+
 
 
 
