32113e832a15ac2f34d632c50adb4720d71f8e6c
pluviophile-WISHER <101853882+pluviophile-WISHER@users.noreply.github.com>
2025-10-27 15:16:27 +0800

    Update Add_data_page.py

--- Add_data_page.py
+++ Add_data_page.py
@@ -8,6 +8,7 @@ import pandas as pd
 from joblib import load
 from pathlib import Path
 import os
+import pkg_resources
 
 def load_model(model_path):
     try:
@@ -503,6 +504,7 @@ def Add_data_file():
             return df
         if st.session_state.show_interpretation:   
             st.markdown("<h4 style='color: rgb(0, 168, 193); font-size: 35px;'>Analysis of blood routine test indicators</h4>", unsafe_allow_html=True)
+
 
 
 
