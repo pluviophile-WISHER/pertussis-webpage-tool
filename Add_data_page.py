c829ce50a2e9088b37bba2d6b4657cfd7b50af02
pluviophile-WISHER <101853882+pluviophile-WISHER@users.noreply.github.com>
2025-10-27 14:51:58 +0800

    Update Add_data_page.py

--- Add_data_page.py
+++ Add_data_page.py
@@ -339,7 +339,7 @@ def Add_data_file():
             st.write(df.to_html(classes='dataframe', index=False, escape=False), unsafe_allow_html=True)
                     
         try:
-            model_path_PH = r"Pertussis data consolidation/XGBoost_model.joblib"  
+            model_path_PH = r"Pertussis data consolidation/PH_XGBoost_model.joblib"  
             model_PH = load(model_path_PH)
         except Exception as e:
             model_PH = None 
@@ -481,6 +481,7 @@ def Add_data_file():
             return df
         if st.session_state.show_interpretation:   
             st.markdown("<h4 style='color: rgb(0, 168, 193); font-size: 35px;'>Analysis of blood routine test indicators</h4>", unsafe_allow_html=True)
+
 
 
 
